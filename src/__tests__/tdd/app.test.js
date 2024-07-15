27adeb59958668888d4cd9c72f63225a0254f000
rpferdner <exploring.art.int@gmail.com>
2024-07-15 18:29:46 -0500

    Add app

---
+++ src/__tests__/tdd/app.test.js
@@ -0,0 +1,8 @@
+// app.test.js
+import { App } from "../../app.js";
+
+describe("App", () => {
+  test("stub", () => {
+    expect(true).toBe(true); // stub
+  });
+});
---
+++ src/app.js
@@ -0,0 +1,4 @@
+// app.js
+import { fsmPattern } from "./patterns.js";
+
+export class App {}
