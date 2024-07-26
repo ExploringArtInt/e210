9866831879a2bec75b1da0f9bf70424fe4326dda
rpferdner <exploring.art.int@gmail.com>
2024-07-26 06:41:27 -0500

    Add card align wihtin app

--- src/content.js
+++ src/content.js
@@ -8,13 +8,36 @@ export function createContent() {
     return;
   }
 
-  const nvpCard = {
+  // card 1
+  const nvpCard1 = {
     zIndex: 1,
   };
 
   console.groupCollapsed("Card");
-  console.table(nvpCard);
+  console.table(nvpCard1);
   console.groupEnd("Card");
 
-  const card = new Card(app, nvpCard);
+  const card1 = new Card(app, nvpCard1);
+
+  // card 2
+  const nvpCard2 = {
+    zIndex: 1,
+  };
+
+  console.groupCollapsed("Card");
+  console.table(nvpCard2);
+  console.groupEnd("Card");
+
+  const card2 = new Card(app, nvpCard2);
+
+  // card 3
+  const nvpCard3 = {
+    zIndex: 1,
+  };
+
+  console.groupCollapsed("Card");
+  console.table(nvpCard3);
+  console.groupEnd("Card");
+
+  const card3 = new Card(app, nvpCard3);
 }
